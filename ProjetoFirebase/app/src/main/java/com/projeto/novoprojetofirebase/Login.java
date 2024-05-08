37f41f2c5b58e8dfc5e45272aaf57b41001d3523
Leo Trevisol <leotrevisol.2021@gmail.com>
2024-05-08 17:38:28 -0300

    Projeto firebase fazendo login e logout de usuario

--- ProjetoFirebase/app/src/main/java/com/projeto/novoprojetofirebase/Login.java
+++ ProjetoFirebase/app/src/main/java/com/projeto/novoprojetofirebase/Login.java
@@ -1,14 +1,89 @@
 package com.projeto.novoprojetofirebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
+
+    private EditText editEmail,editSenha;
+    private CheckBox checkMostrarSenha;
+    private Button btLogin,btRegitsrar;
+    private FirebaseAuth firebaseAuth;
+    private ProgressBar progressBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        editEmail = findViewById(R.id.edit_email);
+        editSenha = findViewById(R.id.edit_senha);
+        btLogin = findViewById(R.id.bt_login);
+        btRegitsrar= findViewById(R.id.bt_registrar);
+        checkMostrarSenha = findViewById(R.id.check_mostrar_senha);
+        progressBar = findViewById(R.id.login_progress_bar);
+
+        btLogin.setOnClickListener(v -> {
+
+            String loginEmail = editEmail.getText().toString();
+            String loginSenha = editSenha.getText().toString();
+
+            if(!TextUtils.isEmpty(loginEmail) || !TextUtils.isEmpty(loginSenha)){
+                progressBar.setVisibility(View.VISIBLE);
+
+                //verifica usuario por email e senha, configurados manualmente no firebase -> Authentication -> Usuários
+                firebaseAuth.signInWithEmailAndPassword(loginEmail,loginSenha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            abrirTelaPrincipal();
+                        }else{
+                            String error = task.getException().getMessage();
+                            Toast.makeText(Login.this, "Erro: " + error, Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.INVISIBLE);
+                        }
+                    }
+                });
+            }
+        });
+
+        checkMostrarSenha.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    editSenha.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                }else{
+                    editSenha.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }
+            }
+        });
+
+    }
+
+    private void abrirTelaPrincipal() {
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
--- ProjetoFirebase/app/src/main/java/com/projeto/novoprojetofirebase/MainActivity.java
+++ ProjetoFirebase/app/src/main/java/com/projeto/novoprojetofirebase/MainActivity.java
@@ -2,14 +2,50 @@ package com.projeto.novoprojetofirebase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btLogout;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btLogout = findViewById(R.id.bt_logout);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+
+        btLogout.setOnClickListener(v -> {
+
+            firebaseAuth.signOut();
+            Intent intent = new Intent(this, Login.class);
+            startActivity(intent);
+            finish();
+
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //verifica se tem usuario logado
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if(currentUser == null){
+            Intent intent = new Intent(this, Login.class);
+            startActivity(intent);
+            finish();
+        }
 
     }
 }
